b2c533f05985ffe2b7d5fad39e70e9f3850e90e0
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-09-28 15:56:04 +0530

    Added success screen

---
+++ app/src/main/java/com/example/e_commercecustomers_ead/activities/SuccessActivity.java
@@ -0,0 +1,16 @@
+package com.example.e_commercecustomers_ead.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_commercecustomers_ead.R;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
